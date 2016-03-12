26e4f8aa52f5eaaaf30a35759c01eb793197ac47
Alex Bateman <bateman311@gmail.com>
2016-03-12 03:17:42 -0700

    created api controller

--- WhoIsWho/APICallController.swift
+++ WhoIsWho/APICallController.swift
@@ -8,3 +8,14 @@
 
 import Foundation
 
+class APIController {
+    
+    func fetchDataFromBamboo (){
+        
+        let stringURL = "https://api.bamboohr.com/api/gateway.php/company/application/json"
+        let url = NSURL(string: stringURL)
+        
+        let data = NSURLSession.sharedSession()
+    }
+    
+}
