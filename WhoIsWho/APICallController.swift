f06851c0955c255b1a30aef0c2997907e11c378d
Alex Bateman <bateman311@gmail.com>
2016-03-12 02:27:39 -0700

    adding controllers

---
+++ WhoIsWho/APICallController.swift
@@ -0,0 +1,10 @@
+//
+//  APICallController.swift
+//  WhoIsWho
+//
+//  Created by Alexander on 3/12/16.
+//  Copyright © 2016 Alex Bateman. All rights reserved.
+//
+
+import Foundation
+
---
+++ WhoIsWho/PhotoAndNameViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoAndNameViewController.swift
+//  WhoIsWho
+//
+//  Created by Alexander on 3/12/16.
+//  Copyright © 2016 Alex Bateman. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAndNameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
